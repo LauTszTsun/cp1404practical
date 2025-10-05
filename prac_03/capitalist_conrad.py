4fba79ad7c082cad27b13f81f62dc377652c8d51
brian <tsztsun.lau@my.jcu.edu.au>
2025-10-05 14:31:23 +0800

    completed with adding open file and outfile with the txt

--- prac_03/capitalist_conrad.py
+++ prac_03/capitalist_conrad.py
@@ -8,7 +8,8 @@ If the price rises above $1000, or falls below $0.01, the program should end.
 The price should be displayed to the nearest cent (e.g. $33.59, not $33.5918232901)
 """
 import random
-
+Filename = "BrianPrac3.txt"
+out_file = open("BrianPrac3.txt", 'w')
 MAX_INCREASE = 0.175  # 17.5%
 MAX_DECREASE = 0.05  # 5%
 MIN_PRICE = 1.0
@@ -16,8 +17,7 @@ MAX_PRICE = 100.0
 INITIAL_PRICE = 10.0
 number_of_days = 0
 price = INITIAL_PRICE
-print(f"Starting price: ${price:,.2f}")
-
+print(f"Starting price: ${price:,.2f}", file=out_file)
 while MIN_PRICE <= price <= MAX_PRICE:
     number_of_days += 1
     price_change = 0
@@ -33,4 +33,5 @@ while MIN_PRICE <= price <= MAX_PRICE:
         price_change = random.uniform(-MAX_DECREASE, 0)
 
     price *= (1 + price_change)
-    print(f"On day {number_of_days} price is ${price:,.2f}")
+    print(f"On day {number_of_days} price is ${price:,.2f}", file=out_file)
+out_file.close()
