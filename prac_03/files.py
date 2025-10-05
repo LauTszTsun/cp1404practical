f7b03f401245b4117b88bd2cd82c2c97e90760be
brian <tsztsun.lau@my.jcu.edu.au>
2025-10-05 17:50:42 +0800

    finished #2 and #3

--- prac_03/files.py
+++ prac_03/files.py
@@ -1,6 +1,22 @@
 #1.
+from fileinput import close
+
 name = input("What is your name? ")
 Filename = "names.txt"
 out_file = open(Filename, "w")
 out_file.write(name)
 out_file.close()
+
+#2.
+in_file = open("names.txt", "r")
+name = in_file.read().strip()
+print(f"Hi {name}")
+in_file.close()
+
+#3.
+with open("numbers.txt", "r") as in_file:
+
+    number1 = int(in_file.readline())
+    number2 = int(in_file.readline())
+result = number1 + number2
+print(result)
