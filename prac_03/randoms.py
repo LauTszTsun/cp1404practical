f742154bd1b80591147aa9137b95c2b94b24fc89
brian <tsztsun.lau@my.jcu.edu.au>
2025-10-05 14:14:30 +0800

    answer the questions and write simple code for random number between 1-100

---
+++ prac_03/randoms.py
@@ -0,0 +1,13 @@
+#What did you see on line 1? 18
+#What was the smallest number you could have seen, what was the largest? 5,20
+
+#What did you see on line 2? 5
+#What was the smallest number you could have seen, what was the largest? 3,9
+#Could line 2 have produced a 4? no
+
+#What did you see on line 3? 5.470990936538749
+#What was the smallest number you could have seen, what was the largest? 2.5,5.5
+
+
+import random
+print(random.randint(1, 100))
