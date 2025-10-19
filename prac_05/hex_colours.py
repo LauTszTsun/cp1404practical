1a7da666bb3791bf5ad170234756f92375423ddb
brian <tsztsun.lau@my.jcu.edu.au>
2025-10-19 15:44:44 +0800

    complete hex_colours

--- prac_05/hex_colours.py
+++ prac_05/hex_colours.py
@@ -8,4 +8,10 @@ COLOR_NAMES ={"AliceBlue": "#f0f8ff",
     "BlanchedAlmond": "#ffebcd",
     "Blue": "#0000ff",
     "BlueViolet": "#8a2be2"}
-
+color_name = input("Enter a color name: ").lower()
+while color_name != "":
+    try:
+        print(f"{color_name} is {COLOR_NAMES[color_name]}")
+    except KeyError:
+        print("Invalid color name")
+    color_name = input("Enter a color name: ").lower()
