ee3c255a53766e2dc91afeaf6f6b304f24077eb9
brian <tsztsun.lau@my.jcu.edu.au>
2025-10-19 15:44:13 +0800

    choose the colours

---
+++ prac_05/hex_colours.py
@@ -0,0 +1,11 @@
+COLOR_NAMES ={"AliceBlue": "#f0f8ff",
+    "AntiqueWhite": "#faebd7",
+    "Aqua": "#00ffff",
+    "Azure": "#f0ffff",
+    "Beige": "#f5f5dc",
+    "Bisque": "#ffe4c4",
+    "Black": "#000000",
+    "BlanchedAlmond": "#ffebcd",
+    "Blue": "#0000ff",
+    "BlueViolet": "#8a2be2"}
+
