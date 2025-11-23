cfe6ad288676b83e65df0bb74b7857629bc96fe3
brian <tsztsun.lau@my.jcu.edu.au>
2025-11-23 10:46:13 +0800

    complete the taxi test, tested at 40 km and restart  and try at 100km

---
+++ prac_09/taxi_test.py
@@ -0,0 +1,13 @@
+from taxi import Taxi
+
+def main():
+    my_taxi = Taxi("Prime 1", 100)
+    my_taxi.drive(40)
+    print(my_taxi)
+    my_taxi.start_fare()
+    my_taxi.drive(100)
+    print(my_taxi)
+
+
+if __name__ == '__main__':
+    main()
